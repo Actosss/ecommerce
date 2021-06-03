66a476dc7188760f00c516cc273ac27bb635914c
kevin <kevinlako88@gmail.com>
2021-06-03 17:33:11 +0300

    update

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { MaterialModule } from '../material/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { AppRoutingModule } from '../app-routing.module';
+
+
+@NgModule({
+  declarations: [HomeComponent,LoginComponent,NavbarComponent,ProfileComponent,RegisterComponent],
+  imports: [CommonModule,MaterialModule,HttpClientModule,ReactiveFormsModule,SharedModule,AppRoutingModule],
+  exports:[HomeComponent,LoginComponent,NavbarComponent,ProfileComponent,RegisterComponent]
+})
+export class PagesModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CartComponent } from './cart/cart.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
+import { AppRoutingModule } from '../app-routing.module';
+
+
+@NgModule({
+  declarations: [CartComponent,TabsComponent,PaginationComponent,CarouselComponent],
+  imports: [CommonModule,MaterialModule,HttpClientModule,ReactiveFormsModule,AppRoutingModule],
+  exports:[CartComponent,TabsComponent,PaginationComponent,CarouselComponent]
+})
+export class SharedModule { }

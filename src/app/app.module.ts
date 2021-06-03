296a5ecc1da056de39a15d83a5f81ba18c9d2e10
kevin <kevinlako88@gmail.com>
2021-06-03 17:32:34 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,34 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NavbarComponent } from './pages/navbar/navbar.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { TabsComponent } from './shared/tabs/tabs.component';
-import { CarouselComponent } from './shared/carousel/carousel.component';
-import { CartComponent } from './shared/cart/cart.component';
-import { PaginationComponent } from './shared/pagination/pagination.component';
 import { CoreModule } from './core/core.module';
 import { authInterceptorProviders } from './core/interceptors/auth.interceptor';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ProfileComponent } from './pages/profile/profile.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule} from '@angular/material/icon';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SharedModule } from './shared/shared.module';
+import { PagesModule } from './pages/pages.module';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    HomeComponent,
-    NavbarComponent,
-    TabsComponent,
-    CarouselComponent,
-    CartComponent,
-    PaginationComponent,
-    ProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -44,7 +28,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     ReactiveFormsModule,
     HttpClientModule,
     MatIconModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    SharedModule,
+    PagesModule
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent]

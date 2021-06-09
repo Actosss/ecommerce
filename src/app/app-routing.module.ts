6abbc31a263fbdd4dd455e7792596ace1273871a
kevin <kevinlako88@gmail.com>
2021-06-09 23:54:54 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,8 +11,8 @@ const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
   { path: "profile", component: ProfileComponent },
-  { path: "cart", component: CartComponent }
-
+  { path: "cart", component: CartComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

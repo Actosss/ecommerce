48a3007a5169254ba81e3131a66541d512cb4193
kevin <kevinlako88@gmail.com>
2021-07-07 20:26:57 +0300

    Update

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
+import { User } from 'src/app/core/interfaces/user';
 import { UserProfile } from 'src/app/core/interfaces/userProfile';
+import { AuthState } from '../login/state/auth.state';
 import { GetUser } from './state/profile.action';
 import { ProfileState } from './state/profile.state';
 
@@ -15,12 +17,13 @@ import { ProfileState } from './state/profile.state';
 export class ProfileComponent implements OnInit {
 
   @Select(ProfileState.userProfile) userProfile$!: Observable<UserProfile>;
+  @Select(AuthState.loggedInUser) loggedInUser$!:Observable<User>;
   userId! :number;
   constructor(private store:Store,private router:Router) {  }
 
   ngOnInit() {
-    this.userProfile$.subscribe(userProfileData => {
-    this.userId = userProfileData.id});
+    this.loggedInUser$.subscribe(userProfile => {
+    this.userId = userProfile.id});
     this.store.dispatch(new GetUser(this.userId));
     this.router.navigate(['/profile']);
   }
